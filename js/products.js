5ed62c935b786722ce7429d6028c82210275065f
Nicolas Garin <88353332+NicoGarin@users.noreply.github.com>
2021-08-10 21:15:05 -0300

    intento fetch

--- js/products.js
+++ js/products.js
@@ -2,5 +2,14 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    let data = getJSONData(PRODUCTS_URL)
+    let row = "";
 
-});
+    row = ` 
+        <tr>
+        <td>`  + data[0].name + ` </td>
+        </tr>
+        ` ;
+
+    document.getElementById("info").innerHTML += row;
+})
